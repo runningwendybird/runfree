b7e70eb9eaae468168cd723d3bf0b94d88086ed1
Hayley Denbraver <runningwendybird@gmail.com>
2015-03-17 22:24:01 -0700

    Fixed some copy on the README and fixed the colors on the moodmap.

--- seedruns.py
+++ seedruns.py
@@ -16,8 +16,7 @@ def seedruns(user_id, number_of_runs_to_add, starting_date, run_id_start):
 	distance_choices = [1.5, 2, 5, 7, 9, 10, 4, 3.1, 6.2, 13.1, 2.25, 8]
 	time_choices = [20, 25, 50, 75, 100, 110, 45, 45, 120, 200, 35, 90]
 	
-	# If you add some routes, you can randomly select a route.
-	# map_choices = [1, 2, 3, 4]
+	map_choices = [1, 2, 3, 4, 5]
 
 	run_id = run_id_start
 
@@ -29,10 +28,10 @@ def seedruns(user_id, number_of_runs_to_add, starting_date, run_id_start):
 		zipcode = random.choice(zipcode_choices)
 		approx_dist = random.choice(distance_choices)
 		approx_time = time_choices[distance_choices.index(approx_dist)]
-		# route_map = random.choice(map_choices)
+		route_map = random.choice(map_choices)
 		commit_date = datetime.datetime.now()
 
-		current_run = model.Run(user_id = user_id, id = run_id, date_run = date_run, zipcode = zipcode, approx_dist = approx_dist, approx_time = approx_time, commit_date = commit_date, route = 0 )
+		current_run = model.Run(user_id = user_id, id = run_id, date_run = date_run, zipcode = zipcode, approx_dist = approx_dist, approx_time = approx_time, commit_date = commit_date, route = route_map )
 		model.sqla_session.add(current_run)
 
 		# will add ratings for the recently added run object. 
@@ -41,19 +40,19 @@ def seedruns(user_id, number_of_runs_to_add, starting_date, run_id_start):
 			# For number ratings
 
 			if question_id == 1:
-				random_rating_number = random.choice([1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5])
+				random_rating_number = random.choice([2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5])
 				current_rating = model.Rating(user_id = user_id, run_id = run_id, question_id = question_id, numeric_ans = random_rating_number)
 
 			if question_id == 2:
-				random_rating_number = random.choice([1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5])
+				random_rating_number = random.choice([ 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5])
 				current_rating = model.Rating(user_id = user_id, run_id = run_id, question_id = question_id, numeric_ans = random_rating_number)
 
 			if question_id == 3:
-				random_rating_number = random.choice([1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5])
+				random_rating_number = random.choice([ 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5])
 				current_rating = model.Rating(user_id = user_id, run_id = run_id, question_id = question_id, numeric_ans = random_rating_number)
 
 			if question_id == 4:
-				random_rating_number = random.choice([1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5])
+				random_rating_number = random.choice([ 2, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5])
 				current_rating = model.Rating(user_id = user_id, run_id = run_id, question_id = question_id, numeric_ans = random_rating_number)
 
 			# For selection ratings. 
